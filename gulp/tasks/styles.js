8a08f1e7e6451c7b27d60604a198fc83929e62cb
Michael-Laptop <micmor1226g@gmail.com>
2019-01-04 17:53:52 -0500

    Changes for lesson 19, What is Mobile First. Added support for mixins.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -10,11 +10,13 @@ var cssVars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 //Allows import to be ignored and replaced with CSS from the CSS module 
 var cssImport = require('postcss-import');
+//Css Mixins. Reusable bit of code 
+var mixins = require('postcss-mixins');
 
 //Automate PostCSS creation
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssVars, nested, autoPrefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoPrefixer]))
     .on('error', function(errorInfo){
         console.log(errorInfo.toString());
         this.emit('end');
