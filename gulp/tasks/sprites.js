bfa9db1e604f979863f03002540eedc694baca62
Michael-Laptop <micmor1226g@gmail.com>
2019-01-24 18:19:46 -0500

    Added changes for part 2 of 3 of the gulp sprites code. Reorganized file structures, and created gulp tasks to move sprites to main assets folder. Reworked the sprite.css template file to better handle creating the sprite.css module. Cleaned up file paths.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: { /*Creates folder called css with svg folder inside and the sprite.css file in it*/
+            sprite: 'sprite.svg', /*Outputs file, removes the .css from the background image url path*/
             render: { /*Render each icon in the sprite with . . .*/
                 css: { /*css in this case. Could use SASS or LESS as well.*/
                     template: './gulp/templates/sprite.css' /*Provides a template to divy out individual images from the sprite file*/
@@ -18,3 +20,19 @@ gulp.task('createSprite', function(){
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+/*Moves the sprite file to the main assets/images/sprites folder to be used.
+This task depends on createSprite.*/
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+/*This task depends on createSprite.*/
+gulp.task('copySpriteCSS', ['createSprite'], function(){
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
