e534b2332234163ca89f48c9e1d2751de40044b2
Michael-Laptop <micmor1226g@gmail.com>
2019-01-30 18:47:00 -0500

    Styled the header section part 1 of 2.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -14,12 +14,14 @@ var nested = require('postcss-nested');
 //We don't want the browser downloading extra files, we want PostCSS to add in the CSS directly from the modules
 //cssImport tells the browser to ignore @import as normally it would be valid CSS
 var cssImport = require('postcss-import');
+//Allows you to declare rgba() function with a hexidecimal color value as opposed to rgba. Also allows that value to be stored in a variable.
+var hexrgba = require('postcss-hexrgba');
 
 
 //Automate PostCSS creation
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssVars, nested, autoPrefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, hexrgba, autoPrefixer]))
     .on('error', function(errorInfo){
         console.log(errorInfo.toString());
         this.emit('end');
